e1a79a914cdb70ab7f48287b11e99dd8b1fe81ea
Ubuntu <ubuntu@ip-172-31-10-0.eu-central-1.compute.internal>
2020-05-30 11:51:25 +0000

    bugfix

--- docker.sh
+++ docker.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get update
-sudo apt-get install \
+apt-get update
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-get install curl -y
 
-sudo apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" -y
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
 
